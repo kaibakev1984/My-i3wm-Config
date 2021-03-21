7aefd04b0d1c6a606a6df7c8bed0fa2bc8af3344
Lorblak <kvasquez@fi.uba.ar>
2021-03-21 13:07:25 -0300

    fix: se eliminaron paquetes deprecados

--- solarized_powerline_beta/scripts/battery_colors.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash 
-
-echo  " Here's my label "  #  full_text 
-echo  #  short_text 
-echo  \#DCFCEC  #  color 
-echo  \#014D20  #  background 
--- solarized_powerline_beta/scripts/fancyscript.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Super fancy script - YAY! I'm a h4x0r!
-echo "$USER"
-echo "Hello I am $USER"
-echo "#E5E5E5"
-exit 0
