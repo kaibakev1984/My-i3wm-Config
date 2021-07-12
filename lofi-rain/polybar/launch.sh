0dc7ef8a81c3f03e6836701a1adf12f93fa3288e
Lorblak <kvasquez@fi.uba.ar>
2021-07-11 23:16:27 -0300

    refactor(lofi-rain): polybar config files were changed

--- lofi-rain/polybar/launch.sh
+++ lofi-rain/polybar/launch.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
+DIR="$HOME/.config/polybar"
 # Terminate already running bar instances
 killall -q polybar
-# If all your bars have ipc enabled, you can also use 
-# polybar-msg cmd quit
 
-# Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar main >>/tmp/polybar1.log 2>&1 & disown
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-echo "Bars launched..."
+# Launch the bar
+polybar -q main -c "$DIR"/config.ini &
